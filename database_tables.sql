75021ddc9ee4407f18b34f1f317dfe2017630a4d
Haridha <36121449+Haridhadeva@users.noreply.github.com>
2018-05-29 12:29:46 +0530

    Create database_tables.sql

---
+++ database_tables.sql
@@ -0,0 +1,8 @@
+create database haridha;
+use haridha;
+create table deva
+(
+slno int,name varchar(50) not null,street varchar(50),district varchar(50),dob int ,state varchar(50),
+religion varchar(20),phoneno int not null,roll_no int ,country varchar(20)
+);
+desc deva;
